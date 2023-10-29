11a435e00e1e558beecc65d6c873b6fdb538ff89
sinceresrishti <136918651+sinceresrishti@users.noreply.github.com>
2023-10-29 23:37:12 +0530

    Add files via upload

---
+++ Arrays Level 1/AnIntroductionToTimeSpaceAndComplexity.cpp
@@ -0,0 +1,29 @@
+// What is Time Complexity?
+// 1. Amount of time taken by an algorithm to run as a function of length of input
+
+// Why to study T&S Complexity?
+// 1.Good software engineer always think about the complexity of code written by her 
+// 2. Resources are limites
+// 3. Measure algorithm to make efficient  program
+// 4. Asked by interviewer after every solution you give
+
+// What is space Complexity?
+// 1. Amount of space taken by an algorithm to run as a function of input
+
+// Unit to represent Complexity
+// 1. Big O: upper bound
+// 2. Theta 0: Average case 
+// Omega: Lower bound
+
+// Big O Complexities:
+// 1. Contant time O(1)
+// 2. Linear time O(n)
+// 3. Logarithmic time O(logN)
+// 4. Quadratic time O(N^2)
+// 5. Cubic time O(N^3)
+
+// O(1), O(logN), O(rootN), O(N), O(nlogn), O(n^2), O(N^3),O(2^n), O(N!), O(N^n)
+// O(1) -> least complex
+// O(N^n) -> most complex
+
+// No. of operation => K = log2N
